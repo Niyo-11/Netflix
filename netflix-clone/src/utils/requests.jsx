9c598af66a4c17888fbf6f5e801d5be6dd4f5042
Niyo-11 <getachewyonas59@gmail.com>
2025-02-19 12:58:12 +0200

    row

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,15 +1,15 @@
 const api_key = import.meta.env.VITE_api_key;
 
 const requests = {
-	fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-US`,
 	fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&with_networks=213`,
+	fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-US`,
 	fetchTopRatedMovies: `/movie/top_rated?api_key=${api_key}&language=en-US`,
-	fetchActionMovies: `/dicover/movie?api_key=${api_key}&with_genres=28`,
-	fetchComedyMovies: `/dicover/movie?api_key=${api_key}&with_genres=35`,
-	fetchHorrorMovies: `/dicover/movie?api_key=${api_key}&with_genres=27`,
-	fetchRomanceMovies: `/dicover/movie?api_key=${api_key}&with_genres=10749`,
-	fetchDocumentaries: `/dicover/movie?api_key=${api_key}&with_genres=99`,
-	fetchTvShows: `/trending/all/week?api_key=${api_key}&language=en-US&page=1`,
+	fetchActionMovies: `/discover/movie?api_key=${api_key}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${api_key}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${api_key}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${api_key}&with_genres=99`,
+	fetchTvShows: `tv/popular?api_key=${api_key}&language=en-US&page=1`,
 };
 
 export default requests;
